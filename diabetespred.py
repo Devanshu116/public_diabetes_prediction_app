83ca5a1a9320ccc57fca79308000875ddf4a2907
Devanshu Sharma <121526636+Devanshu116@users.noreply.github.com>
2023-01-02 15:41:27 -0500

    Add files via upload

---
+++ diabetespred.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import numpy as np
+import pickle
+import streamlit as st
+
+
+# loading the saved model
+
+
+loaded_model = pickle.load(open('diabetes_model.sav','rb'))
+
+
+
+# creating a function for Prediction
+
+
+def diabetes_prediction(input_data):
+    
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return 'The person is not diabetic'
+    else:
+      return 'The person is diabetic'
+
+    
+
+def main():
+    
+    
+    #st.title('Team Bremner')
+    
+    # giving a title
+    st.title('Diabetes Prediction Web App')
+    
+    
+    # getting the input data from the user
+    
+    
+    Pregnancies = st.text_input('Number of Pregnancies', placeholder='Please Enter Value between 0 to 10')
+    Glucose = st.text_input('Glucose Level', placeholder='Please Enter Value between 20 to 200')
+    BloodPressure = st.text_input('Blood Pressure value', placeholder='Please Enter Value between 20 to 150')
+    SkinThickness = st.text_input('Skin Thickness value', placeholder='Please Enter Value between 10 to 100')
+    Insulin = st.text_input('Insulin Level', placeholder='Please Enter Value between 20 to 800')
+    BMI = st.text_input('BMI value', placeholder='Please Enter Value between 5 to 70')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value',placeholder='Please Enter Value between 0 to 3' )
+    Age = st.text_input('Age of the Person', placeholder='Please Enter Value between 21 to 90')
+    
+    
+    # code for Prediction
+    diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        
+        
+    st.success(diagnosis)
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
